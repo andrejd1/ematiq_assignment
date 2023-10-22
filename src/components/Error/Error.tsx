338c0322bd1d607e1c33f491011e97c48d6382e0
Thien Long Ngo <ngo.andrej@gmail.com>
2023-10-22 09:28:16 +0200

    Fix Error.tsx links

--- src/components/Error/Error.tsx
+++ src/components/Error/Error.tsx
@@ -8,9 +8,12 @@ function Error() {
       <h3>Each value must be in range &lt;0; 100&gt;</h3>
       (i.e. &nbsp;
       <a
-        href={`http://${window.location.hostname}:${window.location.port}/${BASE_PATH}/?q=100-45-21-60-24`}
+        href={`http://${window.location.hostname}${
+          window.location.port ? `:${window.location.port}` : ''
+        }/${BASE_PATH}/?q=100-45-21-60-24`}
       >
-        http://{window.location.hostname}:{window.location.port}/${BASE_PATH}
+        http://{window.location.hostname}
+        {window.location.port ? `:${window.location.port}` : ''}/{BASE_PATH}
         /?q=100-45-21-60-24
       </a>
       )
